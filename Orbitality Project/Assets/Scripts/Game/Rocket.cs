b4daf908a6f70c1d324ab0b12e6e003119e983c4
zxzfranszxz <game.frans@gmail.com>
2019-08-03 21:55:50 +0300

    Add Rocket.cs and RocketSO.cs. Create RocketSO objects

---
+++ Orbitality Project/Assets/Scripts/Game/Rocket.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class Rocket : MonoBehaviour
+{
+    private RocketSO rocketSO;
+
+    Rigidbody rigidBody;
+
+    float timeToDeath = 10;
+
+    void Awake()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+        
+        //rigidBody.maxAngularVelocity = 4f;
+    }
+
+    private void Start()
+    {
+        rigidBody.velocity = transform.forward * Data.planetSpeed * (rocketSO.Acceleration / 10f);
+    }
+
+    public RocketSO RocketSO
+    {
+        set
+        {
+            rocketSO = value;
+
+            transform.localScale = new Vector3(1, 1, 1) * Mathf.Sqrt(rocketSO.Weight);
+            rigidBody.mass = rocketSO.Weight;
+        }
+    }
+
+    void Update()
+    {
+        if (rocketSO)
+        {
+            rigidBody.AddForce(transform.forward * rocketSO.Acceleration * Time.deltaTime, ForceMode.Acceleration);
+
+            transform.rotation = Quaternion.LookRotation(rigidBody.velocity);
+        }
+
+        timeToDeath -= Time.deltaTime;
+        if (timeToDeath < 0)
+            Destroy(gameObject);
+
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "planet")
+        {
+            PlanetView planetView = collision.gameObject.GetComponent<PlanetView>();
+            if(planetView && rocketSO)
+            {
+                planetView.Damage(rocketSO.Damage);
+            }
+        }
+        if (collision.gameObject.tag == "rocket")
+            return;
+
+        Destroy(gameObject);
+    }
+
+
+}
---
+++ Orbitality Project/Assets/Scripts/SO/RocketSO.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[CreateAssetMenu(fileName = "RocketSO", menuName = "RocketSO")]
+public class RocketSO : ScriptableObject
+{
+    [SerializeField]
+    private int damage;
+    [SerializeField]
+    private float acceleration;
+    [SerializeField]
+    private float weight;
+    [SerializeField]
+    private float cooldown;
+
+    public int Damage { get { return damage; } }
+    public float Acceleration { get { return acceleration; } }
+    public float Weight { get { return weight; } }
+    public float Cooldown { get { return cooldown; } }
+}

dd9c208283379a7811659b4a67d9dd88633c25c5
zxzfranszxz <game.frans@gmail.com>
2019-08-03 22:01:26 +0300

    Add Data.cs and ResourcesData.cs

---
+++ Orbitality Project/Assets/Scripts/Data/Data.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Data
+{
+    public const int planetMaxHP = 100;
+    public const int minOrbitRadius = 20;
+    public const int orbitStep = 4;
+    public const int planetSize = 4;
+    public const int planetSpeed = 20;
+
+}
---
+++ Orbitality Project/Assets/Scripts/Data/ResourcesData.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ResourcesData
+{
+    public const string planetPath = "3D/Planet/Planet";
+    public const string rocketsPath = "SO/Rockets";
+    public const string rocketPrefabPath = "3D/Rocket/Rocket";
+}

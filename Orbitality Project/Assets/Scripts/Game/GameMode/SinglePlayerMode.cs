cd3a9420a6081c5909c8ee0b9250327d2f5697b0
zxzfranszxz <game.frans@gmail.com>
2019-08-03 20:05:02 +0300

    Add SinglePlayerMode.cs and GameStarter.cs

---
+++ Orbitality Project/Assets/Scripts/Game/GameMode/SinglePlayerMode.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SinglePlayerMode
+{
+    public const int minEnemies = 1;
+    public const int maxEnemies = 5;
+    public int Enemies { get; protected set; }
+
+
+    public SinglePlayerMode(int min, int max)
+    {
+        Enemies = Random.Range(min, max + 1);
+    }
+
+    
+
+    public static SinglePlayerMode CreateSinglePlayerMode(int min, int max)
+    {
+        if (min < minEnemies || max > maxEnemies || min > max)
+            return null;
+
+        return new SinglePlayerMode(min, max);
+    }
+
+
+}
---
+++ Orbitality Project/Assets/Scripts/Game/GameStarter.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class GameStarter
+{
+    public SinglePlayerMode SinglePlayerMode { get; protected set;}
+
+    public GameStarter()
+    {
+        SinglePlayerMode = new SinglePlayerMode(4,5);
+    }
+
+    public void StartGame(SinglePlayerMode singlePlayerMode)
+    {
+
+        SinglePlayerMode = singlePlayerMode;
+
+        SceneManager.LoadScene("SingleMode");
+    }
+    
+
+}

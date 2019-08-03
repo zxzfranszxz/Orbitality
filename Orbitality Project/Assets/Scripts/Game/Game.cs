b1a3f919ee039aaa8aeecf4c879e8dc61b976161
zxzfranszxz <game.frans@gmail.com>
2019-08-03 19:50:29 +0300

    Add Game.cs
    Game.cs has been added and set it execution order to -100.
    Game.cs is application entry point.
    Also MainMenuCamera.cs has been moved to MainMenu/MainMenuCamera.cs.

---
+++ Orbitality Project/Assets/Scripts/Game/Game.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour
+{
+
+    private static Game instance;
+
+
+    void Awake()
+    {
+
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        DontDestroyOnLoad(this);
+
+        Init();
+
+
+        
+    }
+
+    private void Init()
+    {
+
+    }
+    
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void Pause(bool pause)
+    {
+        Time.timeScale = pause ? 0 : 1;
+    }
+
+    public static Game Instance
+    {
+        get
+        {
+            return instance;
+        }
+    }
+}

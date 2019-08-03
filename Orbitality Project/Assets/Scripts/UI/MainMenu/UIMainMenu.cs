e755bba5c8ccb9bc515c08f19d50a5d98b9feda8
zxzfranszxz <game.frans@gmail.com>
2019-08-03 20:08:37 +0300

    Add UIMainMenu

---
+++ Orbitality Project/Assets/Scripts/UI/MainMenu/UIMainMenu.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIMainMenu : MonoBehaviour
+{
+    [SerializeField]
+    private Button playButton;
+
+    [SerializeField]
+    private Button loadButton;
+
+    [SerializeField]
+    private Button quitButton;
+    
+
+    private void Start()
+    {
+        playButton.onClick.AddListener(OnPlayClick);
+        loadButton.onClick.AddListener(OnLoadClick);
+        quitButton.onClick.AddListener(OnQuitClick);
+        
+    }
+
+    public void OnPlayClick()
+    {
+        
+    }
+
+    public void OnLoadClick()
+    {
+
+    }
+
+    public void OnQuitClick()
+    {
+        Game.Instance.Quit();
+    }
+}

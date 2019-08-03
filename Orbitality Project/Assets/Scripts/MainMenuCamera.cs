23a80cc88baa45fa4d788a2f8fa691cf4e502c2a
zxzfranszxz <game.frans@gmail.com>
2019-08-03 19:33:22 +0300

    Add MainMenuCamera

---
+++ Orbitality Project/Assets/Scripts/MainMenuCamera.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuCamera : MonoBehaviour
+{
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.rotation = Quaternion.Euler(0, Mathf.Sin(Time.time / 2) * 4, 0);
+    }
+}

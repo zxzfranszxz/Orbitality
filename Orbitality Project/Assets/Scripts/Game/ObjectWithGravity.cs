95c47655964fb849744ea14183dcf865188e28fb
zxzfranszxz <game.frans@gmail.com>
2019-08-03 21:41:13 +0300

    Add ObjectWithGravity.cs

---
+++ Orbitality Project/Assets/Scripts/Game/ObjectWithGravity.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectWithGravity : MonoBehaviour
+{
+    public float gravityPower;
+    public float gravityRange;
+
+
+    int layerMask; 
+    void Start()
+    {
+        layerMask = 1 << 8;
+    }
+
+    public void FixedUpdate()
+    {
+        foreach (Collider collider in Physics.OverlapSphere(transform.position, gravityRange, layerMask))
+        {
+
+            Vector3 forceDirection = transform.position - collider.transform.position;
+            
+            collider.attachedRigidbody.AddForce(forceDirection.normalized * gravityPower * collider.attachedRigidbody.mass, ForceMode.Acceleration);
+        }
+    }
+}
